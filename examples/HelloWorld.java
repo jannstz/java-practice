61435bd82638cffa54eec1fbc637bb5a5aa31936
jannstoltz <jannstz@gmail.com>
2017-07-20 12:56:57 -0500

    feat: Create HelloWorld example
    Example shows structure of simple Java class and main() method and
    prints the classic 'Hello, World!' message to the console.

---
+++ examples/HelloWorld.java
@@ -0,0 +1,19 @@
+/*
+    This is a simple Java program.
+    IMPORTANT: File is named for the class below.
+*/
+
+class HelloWorld {
+    // Java programs begin with a call to main().
+    public static void main(String args[]) {
+        // public means this method is accessible from outside this class
+        // static means this method is available before instantiation
+        // void means the return type for this method is NullPointerException
+        // main is the name of the method
+        // String args[] is the argument or parameters passed to the main method
+        System.out.println("Hello, World!");
+        // System.out.println is a method call that prints a line to the console
+        // Arguments for this method must be a string, or a data type that can
+        //     be converted to a string, such as int
+    }
+}
